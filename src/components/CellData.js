e7d4e2d743f70c8fda205127e8601518048dde0a
MatthewSzurkowski <mszurkow@gmail.com>
2023-06-13 21:23:14 -0400

    Modal development, styling and component design + general fixes. Maingrid Cell component development + design.

---
+++ src/components/CellData.js
@@ -0,0 +1,137 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar, faCloudDownload, faPencil, faUsers, faLaptop, faCommentAlt, faColumns, faCow, faGlobeAmericas} from '@fortawesome/free-solid-svg-icons';
+import { Button, ButtonGroup, Container } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
+
+import "./styles/CellData.css";
+
+function ShortenDesc(text){
+    const maxLength = 250;
+    if (text.length <= maxLength) {
+        return text;
+    } else {
+        const shortenedText = text.substr(0, maxLength);
+        const lastWordBoundary = shortenedText.lastIndexOf(' ');
+        return shortenedText.substr(0, lastWordBoundary) + "...";
+    }
+}
+
+function CellData(props){
+    const descText = ShortenDesc(props.desc);
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = (event) => {
+        // const div = event.currentTarget;
+        // const edit = div.querySelector('.edit');
+        // console.log("Div: ");
+        // console.log(div);
+        // console.log("Edit: ");
+        // console.log(edit);
+        // if (event.currentTarget !== event.target) {
+        //     return;
+        // }
+        return setShow(true);
+    };
+    return(
+        <>
+        <div>
+            <div onClick={handleShow} className='title-and-desc'>
+                <p><b>{props.title}</b></p>
+                <p>{descText}</p>
+            </div>
+            <div className='align-year-and-link'>
+                <div className='align-icon-left'>
+                    <FontAwesomeIcon icon={faCalendar} style={{ color: 'black', marginRight: "20%", marginTop: "8%" }}/>
+                    <p>{props.year}</p>
+                </div>
+                <div id="quick-link-download">
+                    <a href={props.csvDownloadlink} download>CSV
+                        <FontAwesomeIcon icon={faCloudDownload} style={{ color: 'black' }}/>
+                    </a>
+                </div>
+            </div>
+        </div>
+        {/* title="FAOSTAT Crops and livestock products - Stocks"
+                            desc="Livestock population statistics covering the following categories: Animals live n.e.s.; Asses; Beehives; Buffaloes; Camelids, other; Camels; Cattle; Chickens; Ducks; Geese and guinea fowls; Goats; Horses; Mules; Pigeons, other birds; Pigs; Rabbits and hares; Rodents, other; Sheep; Turkeys."
+                            year= "2021" 
+                            csvDownloadlink="google.com" 
+                            nameOfDataSet="Example Name of Dataset"
+                            authors="Kassy Raymond, Deb Stacey, Matthew Szurkowski"
+                            nameOfDataSource="Example name of data source"
+                            endYear="2018"
+                            tableName="Example Table Name"
+                            apiCall="Example API Call script"
+                            metadataDownloadLink="google.com"
+                            columnsIncluded="Example columns included"
+                            measured="Example species 1, species 2, etc"
+                            spatialRange="Example spacial range" */}
+        <Modal show={show} onHide={handleClose} animation={true} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>{props.nameOfDataSet}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <div className='split-modal'>
+                <div>
+                    <div className='left-align-modal-icons'>
+                        <FontAwesomeIcon icon={faPencil} style={{ color: 'black', marginTop: "1%", marginRight: "1%" }}/>
+                        <p><b>Table Name:</b> {props.tableName}</p>
+                    </div>
+                    <div className='left-align-modal-icons'>
+                        <FontAwesomeIcon icon={faUsers} style={{ color: 'black', marginTop: "1%", marginRight: "1%" }}/>
+                        <p><b>Authors:</b> {props.authors}</p>
+                    </div>
+                    <div className='left-align-modal-icons'>
+                        <FontAwesomeIcon icon={faLaptop} style={{ color: 'black', marginTop: "1%", marginRight: "1%" }}/>
+                        <p><b>Data source:</b> {props.nameOfDataSource}</p>
+                    </div>
+                    <div className='left-align-modal-icons'>
+                        <FontAwesomeIcon icon={faCalendar} style={{ color: 'black', marginTop: "1%", marginRight: "1%" }}/>
+                        <p><b>End year:</b> {props.endYear}</p>
+                    </div>
+                    <Container style={{textAlign: "center"}}>
+                        <ButtonGroup className="mb-2">
+                            <Button variant="outline-primary" onClick={handleClose}>
+                                CSV
+                            </Button>
+                            <Button variant="outline-secondary" onClick={handleClose}>
+                                API CALL
+                            </Button>
+                            <Button variant="outline-secondary" onClick={handleClose}>
+                                Download metadata
+                            </Button>
+                        </ButtonGroup>
+                    </Container>
+                    <hr/>
+                    <div className='left-align-modal-icons'>
+                        <FontAwesomeIcon icon={faCommentAlt} style={{ color: 'black', marginTop: "1%", marginRight: "1%" }}/>
+                        <p><b>Description:</b> {props.desc}</p>
+                    </div>
+                    <div className='left-align-modal-icons'>
+                        <FontAwesomeIcon icon={faColumns} style={{ color: 'black', marginTop: "1%", marginRight: "1%" }}/>
+                        <p><b>Columns included:</b> {props.columnsIncluded}</p>
+                    </div>
+                    <div className='left-align-modal-icons'>
+                        <FontAwesomeIcon icon={faCow} style={{ color: 'black', marginTop: "1%", marginRight: "1%" }}/>
+                        <p><b>Measured species:</b> {props.measured}</p>
+                    </div>
+                    <div className='left-align-modal-icons'>
+                        <FontAwesomeIcon icon={faGlobeAmericas} style={{ color: 'black', marginTop: "1%", marginRight: "1%" }}/>
+                        <p><b>Spatial Range:</b> {props.spatialRange}</p>
+                    </div>
+                </div>
+                <div>ASK DEB WHAT SHE WANTS HERE</div>
+            </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+        </>
+    );
+}
+
+export default CellData;
--- src/components/MainGrid.js
+++ src/components/MainGrid.js
@@ -1,52 +1,104 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import { ButtonGroup, Container } from 'react-bootstrap';
 import "./styles/MainGrid.css";
 // import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import ToggleButton from 'react-bootstrap/ToggleButton';
+// import ToggleButton from 'react-bootstrap/ToggleButton';
+
+import CellData from './CellData';
 
 function MainGrid() {
-    const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-    const [checked, setChecked] = useState(false);
+    // const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+     
+    // const [checked, setChecked] = useState(false);
     return (
         <Container className='main-table'>
-            <ButtonGroup>
-                <ToggleButton
-                    // className="mb-2"
-                    id="toggle-check"
-                    type="checkbox"
-                    variant="outline-primary"
-                    checked={checked}
-                    value="1"
-                    onChange={(e) => setChecked(e.currentTarget.checked)}
-                > Has PDF
-                </ToggleButton>
-                <Button variant="outline-secondary">Filter</Button>{' '}
-                <Button variant="outline-secondary">Sort By</Button>{' '}
-                <Button variant="outline-secondary">Export As</Button>{' '}
-            </ButtonGroup>
+            <div className="d-flex justify-content-end">
+                <ButtonGroup>
+                    {/* <ToggleButton
+                        // className="mb-2"
+                        id="toggle-check"
+                        type="checkbox"
+                        variant="outline-primary"
+                        checked={checked}
+                        value="1"
+                        onChange={(e) => setChecked(e.currentTarget.checked)}
+                    > Has PDF
+                    </ToggleButton> */}
+                    <Button variant="outline-secondary">Filter</Button>{' '}
+                    <Button variant="outline-secondary">Sort By</Button>{' '}
+                    <Button variant="outline-secondary">Export As</Button>{' '}
+                </ButtonGroup>
+            </div>
             <Table responsive>
-            <thead>
-                <tr>
-                <th>Paper Title</th>
-                <th>Abstract Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{lorem}</td>
-                    <td>{lorem}</td>
-                </tr>
-                <tr>
-                    <td>{lorem}</td>
-                    <td>{lorem}</td>
-                </tr>
-                <tr>
-                    <td>{lorem}</td>
-                    <td>{lorem}</td>
-                </tr>
-            </tbody>
+                <thead>
+                    <tr>
+                        <th style={{color: "grey"}}>Paper title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className='parent-cell'>
+                            <CellData 
+                            title="FAOSTAT Crops and livestock products - Stocks"
+                            desc="Livestock population statistics covering the following categories: Animals live n.e.s.; Asses; Beehives; Buffaloes; Camelids, other; Camels; Cattle; Chickens; Ducks; Geese and guinea fowls; Goats; Horses; Mules; Pigeons, other birds; Pigs; Rabbits and hares; Rodents, other; Sheep; Turkeys."
+                            year= "2021" 
+                            csvDownloadlink="google.com" 
+                            nameOfDataSet="Example Name of Dataset"
+                            authors="Kassy Raymond, Deb Stacey, Matthew Szurkowski"
+                            nameOfDataSource="Example name of data source"
+                            endYear="2018"
+                            tableName="Example Table Name"
+                            apiCall="Example API Call script"
+                            metadataDownloadLink="google.com"
+                            columnsIncluded="Example columns included"
+                            measured="Example species 1, species 2, etc"
+                            spatialRange="Example spacial range"
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                    <td className='parent-cell'>
+                            <CellData 
+                            title="FAOSTAT Crops and livestock products - Stocks"
+                            desc="Livestock population statistics covering the following categories: Animals live n.e.s.; Asses; Beehives; Buffaloes; Camelids, other; Camels; Cattle; Chickens; Ducks; Geese and guinea fowls; Goats; Horses; Mules; Pigeons, other birds; Pigs; Rabbits and hares; Rodents, other; Sheep; Turkeys."
+                            year= "2021" 
+                            csvDownloadlink="google.com" 
+                            nameOfDataSet="Example Name of Dataset"
+                            authors="Kassy Raymond, Deb Stacey, Matthew Szurkowski"
+                            nameOfDataSource="Example name of data source"
+                            endYear="2018"
+                            tableName="Example Table Name"
+                            apiCall="Example API Call script"
+                            metadataDownloadLink="google.com"
+                            columnsIncluded="Example columns included"
+                            measured="Example species 1, species 2, etc"
+                            spatialRange="Example spacial range"
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                    <td className='parent-cell'>
+                            <CellData 
+                            title="FAOSTAT Crops and livestock products - Stocks"
+                            desc="Livestock population statistics covering the following categories: Animals live n.e.s.; Asses; Beehives; Buffaloes; Camelids, other; Camels; Cattle; Chickens; Ducks; Geese and guinea fowls; Goats; Horses; Mules; Pigeons, other birds; Pigs; Rabbits and hares; Rodents, other; Sheep; Turkeys."
+                            year= "2021" 
+                            csvDownloadlink="google.com" 
+                            nameOfDataSet="Example Name of Dataset"
+                            authors="Kassy Raymond, Deb Stacey, Matthew Szurkowski"
+                            nameOfDataSource="Example name of data source"
+                            endYear="2018"
+                            tableName="Example Table Name"
+                            apiCall="Example API Call script"
+                            metadataDownloadLink="google.com"
+                            columnsIncluded="Example columns included"
+                            measured="Example species 1, species 2, etc"
+                            spatialRange="Example spacial range"
+                            />
+                        </td>
+                    </tr>
+                </tbody>
             </Table>
         </Container>
     );
