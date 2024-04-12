c59e723ce6a7b587109c8f6012b02ba37aa70668
MatthewSzurkowski <mszurkow@gmail.com>
2024-04-12 18:49:38 -0400

    Fixing log messages display.

--- src/pages/Logs.js
+++ src/pages/Logs.js
@@ -23,37 +23,43 @@ function Logs(){
 
     fetchLogsFromAPI();
 
-    const messages = logs.split('\n');
+    var messages = logs.split('\n').filter(message => message.trim() !== '');
+    messages = messages.reverse();
 
-    return(
-        <div>
-            <h1 style={{textAlign: "center"}}>GBADs TAIL Logs</h1>
-            <hr style={{marginLeft: "auto", marginRight: "auto", width: "50%"}}/>
-            <div style={{
-    display: "flex",
-    alignItems: "center", // Center vertically
-    flexDirection: "column",
-    width: "80%", // Limit the width of the container
-    margin: "auto", // Center horizontally
-}}>
-    {messages.map((message, index) => (
-        <p 
-            key={index} 
-            style={{
+    return (
+    <div>
+        <h1 style={{ textAlign: "center", marginTop: "5%" }}>GBADs TAIL Logs</h1>
+        <hr style={{ marginLeft: "auto", marginRight: "auto", width: "50%" }} />
+        <div style={{
+        display: "flex",
+        alignItems: "center", // Center vertically
+        flexDirection: "column",
+        width: "80%", // Limit the width of the container
+        margin: "auto", // Center horizontally
+        }}>
+        {messages.length === 0 || messages[0] === "" ? (
+            <p style={{ textAlign: "center", marginBottom: "300px" }}>
+            No logs yet. Make a request on <a href="https://gbadske.org/search">https://gbadske.org/search</a>
+            </p>
+        ) : (
+            messages.map((message, index) => (
+            <p
+                key={index}
+                style={{
                 textAlign: "center",
                 border: "1px solid black",
-                width: "100%", // Ensure all elements have the same width
-                backgroundColor: index % 2 === 0 ? "orange" : "gray", // Alternate colors for every two messages
+                width: "50%", // Ensure all elements have the same width
+                backgroundColor: index % 2 === 0 ? "#DCDCDC" : "#F5D5A5", // Alternate colors for every two messages
                 margin: "0", // Remove margin to ensure elements touch each other
                 padding: "10px" // Add padding for spacing between text and border
-            }}
-        >
-            {message}
-        </p>
-    ))}
-</div>
-
+                }}
+            >
+                {message}
+            </p>
+            ))
+        )}
         </div>
+    </div>
     );
 }
 
